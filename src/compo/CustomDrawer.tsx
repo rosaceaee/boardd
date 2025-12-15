174a858a4a7deb0e26fcffb4be105d7eacf2f3cb
장영주 <corydalisss@gmail.com>
2025-12-15 17:22:44 +0900

    [ui] 抜いちゃったコンポーネント追加

--- src/compo/CustomDrawer.tsx
+++ src/compo/CustomDrawer.tsx
@@ -6,7 +6,8 @@ import {
   FileTextOutlined,
   FolderOpenOutlined,
 } from "@ant-design/icons";
-import { Avatar } from "antd";
+import { Avatar, Form, Input, Button } from "antd";
+import Box from "./Box";
 
 interface dataTypes {
   setOpen: boolean;
@@ -25,13 +26,93 @@ const CustomDrawer = ({ title, children }: dataTypes) => {
     <>
       {!open && (
         <div className="chat-detail">
-          <p onClick={closeThis}>BACK</p>
-          <p style={{ border: " 2px solid red" }}>
-            <Avatar size={46} icon={<UserOutlined />} />
-            {title}
-            <br />
-            {children}
-          </p>
+          <div className="usr-info">
+            <div className="info-box">
+              <Avatar size={46} icon={<UserOutlined />} />
+              {title}
+              <br />
+              {children}
+            </div>
+            <p className="back" onClick={closeThis}>
+              BACK
+            </p>
+          </div>
+          {/* // */}
+          <div className="chat-wrap">
+            <div className="usr-chat-box">
+              <Avatar
+                size={34}
+                icon={<UserOutlined />}
+                style={{ flexShrink: 0 }}
+              />
+              <Box
+                radius={10}
+                className="bg received"
+                widthh={0}
+                background={`#fff`}
+                padding={0}
+              >
+                <p>annyeong haseyong</p>
+              </Box>
+            </div>
+
+            <div className="usr-chat-box reverse">
+              <Box
+                radius={10}
+                className="bg sent"
+                widthh={0}
+                background={`#fff`}
+                padding={0}
+              >
+                <p>annyeong haseyong</p>
+              </Box>
+            </div>
+            <div className="usr-chat-box reverse">
+              <Box
+                radius={10}
+                className="bg sent"
+                widthh={0}
+                background={`#fff`}
+                padding={0}
+              >
+                <p>
+                  annyeong haseyong annyeong haseyong annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                  <br />
+                  annyeong haseyong
+                </p>
+              </Box>
+            </div>
+            {/* // */}
+          </div>{" "}
+          <div className="textarea">
+            <Form.Item name={["user", "introduction"]}>
+              <Input.TextArea placeholder="dd" />
+              <Button type="primary"> send</Button>
+            </Form.Item>
+          </div>
+          {/* // */}
         </div>
       )}
     </>
